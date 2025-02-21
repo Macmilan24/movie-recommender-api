358e5bcb9be32c5ace8e998694fad905123e30e4
Macmilan24 <cpt.macmilan24@gmail.com>
2025-02-21 17:55:37 +0300

    [FIX]: CORS bug

--- movie_recommender_api.py
+++ movie_recommender_api.py
@@ -23,7 +23,7 @@ app = FastAPI(title="Movie Recommender API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://movie-recommender-tester.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://movie-recommender-tester.vercel.app","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
